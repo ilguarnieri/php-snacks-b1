5e5fbb744a3141d22d167dce8a64edc2ed29f11c
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-04-01 01:31:52 +0200

    snack 4

--- snack_4.php
+++ snack_4.php
@@ -1,5 +1,15 @@
 <?php
-    echo 'Snack 4';
 
+    $numbers = [];
 
+    do{
+        $num = rand(1, 100);
+
+        if(!in_array($num, $numbers)){
+            $numbers[] = $num;
+        };
+
+    }while(count($numbers) < 15);
+
+    var_dump($numbers);
 ?>
