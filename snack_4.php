ffa533c6d54d13c768f6d67295ef23921290c926
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-04-01 01:59:12 +0200

    insert html snack 4

--- snack_4.php
+++ snack_4.php
@@ -10,6 +10,36 @@
         };
 
     }while(count($numbers) < 15);
-
-    var_dump($numbers);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 4</title>
+</head>
+<body>
+
+    <ol>
+        <?php
+        for($i = 0; $i < count($numbers); $i++){
+        ?>
+
+        <li>
+            <span style="font-weight: bold;">
+                <?php
+                echo $numbers[$i];
+                ?>
+            </span>
+        </li>
+
+        <?php
+        }
+        ?>
+    </ol>
+    
+</body>
+</html>
