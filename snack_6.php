fab918eb934e5d33ec5e537f1990d155f0df8fbf
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-04-01 11:21:59 +0200

    snack 6

--- snack_6.php
+++ snack_6.php
@@ -1,5 +1,91 @@
-<?php
-    echo 'Snack 6';
-
-
+<?php 
+    $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+    ];
+    
+    $keys = array_keys($db);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 6</title>
+
+    <style>
+        h3{
+            text-align: center;
+            color: red;
+        }
+        .container{
+            border: 1px solid black;
+            margin-bottom: 20px;
+            max-width: 300px;
+            font-weight: bold;
+        }
+        .teachers{
+            background-color: #ccc;
+        }
+        .pm{
+            background-color: #88cc88;
+        }
+    </style>
+</head>
+<body>
+
+    <?php
+    for($i = 0; $i < count($keys); $i++){
+        $genre = $keys[$i];
+        $people = $db[$genre];
+    ?>
+
+    <div class="container <?php echo $genre; ?>">
+
+        <h3><?php echo ucfirst($genre); ?></h3>
+
+        <ul>
+
+            <?php
+             for($j = 0; $j < count($people); $j++){
+                $person = $people[$j];
+            ?>
+
+            <li>
+                <?php
+                echo "{$person['name']} {$person['lastname']}";
+                ?>
+            </li>
+
+            <?php
+            }
+            ?>
+        </ul>
+    </div>
+
+    <?php
+    }
+    ?>    
+    
+</body>
+</html>
